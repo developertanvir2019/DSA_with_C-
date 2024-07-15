971d6d60ef9f2085a7abe34ae7de9417cca976dc
developertanvir2019 <developertanvir2019@gmail.com>
2024-07-15 23:55:57 +0600

    complete assignment

---
+++ Special_Queries.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int q;
+    cin >> q;
+    queue<string> qu;
+
+    for (int i = 0; i < q; ++i) {
+        int command;
+        cin >> command;
+        if (command == 0) {
+            string name;
+            cin >> name;
+            qu.push(name);
+        } else if (command == 1) {
+            if (!qu.empty()) {
+                cout << qu.front() << endl;
+                qu.pop();
+            } else {
+                cout << "Invalid" << endl;
+            }
+        }
+    }
+
+    return 0;
+}
