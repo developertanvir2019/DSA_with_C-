84632105d0262c6eb8790e65a1dbaa1fd468a33a
developertanvir2019 <developertanvir2019@gmail.com>
2024-08-12 13:58:25 +0600

    practicing recap

---
+++ recap_module/session1.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Node{
+    public:
+        int val;
+        Node * next;
+        Node(int val){
+            this->val=val;
+            this->next=NULL;
+        }
+};
+void printList(Node*head){
+    while(head!=NULL){
+        cout<<head->val<<" ";
+        head=head->next;
+    }
+    cout<<endl;
+}
+void insertTail(int val,Node*& head){
+Node *newNode=new Node(val);
+if(head==NULL){
+    head=newNode;
+    return;
+}
+Node *tmp=head;
+while(tmp->next!=NULL){
+    tmp=tmp->next;
+}
+tmp->next=newNode;
+}
+
+int main (){
+    Node *a =new Node(1);
+    Node*b=new Node(2);
+    Node*c=new Node(3);
+    a->next=b;
+    b->next=c;
+    printList(a);
+    return 0;
+} 
