eb5bb69e0c21830d0241428c9c2dbe2266c7c694
developertanvir2019 <developertanvir2019@gmail.com>
2024-10-09 15:03:42 +0600

    reviced some data structure

--- assignment1/Get_Prefix_Sum.cpp
+++ assignment1/Get_Prefix_Sum.cpp
@@ -4,18 +4,19 @@ using namespace std;
 int main (){
     int n;
     cin>>n;
-    long long a[n];
+    vector<long long> a(n);
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
-    long long pre[n];
-  pre[0]=a[0];
+     vector<long long>  pre(n);
+     pre[0]=a[0];
     for(int i=1;i<n;i++){
-    pre[i]=a[i]+pre[i-1];
+      pre[i]=a[i]+pre[i-1];
     }
-    sort(pre,pre+n,greater<long long>());
+    sort(pre.begin(),pre.end(),greater<long long>());
+
     for(int i=0;i<n;i++){
-  cout<<pre[i]<<" ";
+      cout<<pre[i]<<" ";
     }
     return 0;
 }
